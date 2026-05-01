73bc1c38297b300e685d6b74903e2f545adc7a0b
V-Anirudh <vallampatlaanirudh@gmail.com>
2026-04-30 23:56:54 -0400

    Add @emailjs/browser dependency

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  emailJs: {
+    serviceId: 'service_kcrcj4q',
+    templateId: '__ejs-test-mail-service__',
+    publicKey: '2Q3NYv3-OMi1UNSAt'
+  }
+};
